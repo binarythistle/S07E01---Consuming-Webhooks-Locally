a25a69b323ff907a99304e812ec56bbe214e1e2b
binarythistle <les.jackson@binarythistle.com>
2024-07-15 19:04:28 +1000

    Initial Commit

---
+++ LocalWebhookEndpoint/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+
+
+app.MapPost("/githubwebhooks", (HttpContext context) =>
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("--> We got a hit");
+    var headers = context.Request.Headers;
+    foreach (var header in headers)
+    {
+        Console.WriteLine($"{header.Key}: {header.Value}");
+    }
+
+    Console.ResetColor();
+    return Results.Ok("Got a Webhook");
+});
+
+app.Run();
